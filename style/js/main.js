d676ded6791fd1d1c5420eb0f564543b9f38ec41
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-06-15 15:57:07 +0530

    js

--- style/js/main.js
+++ style/js/main.js
@@ -1,5 +1,5 @@
 let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+let buttons = document.querySelectorAll(`button`);
 
 let string = "";
 let arr = Array.from(buttons);
